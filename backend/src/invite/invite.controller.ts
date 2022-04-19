27edb021b76d7f03dff0559412d0cb7590f2bfe6
Maximilian Trimarchi <maxipaltri@gmail.com>
2022-04-19 15:25:55 +0200

    invites route improved

--- backend/src/invite/invite.controller.ts
+++ backend/src/invite/invite.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Header, Post } from '@nestjs/common';
 import { InviteService } from './invite.service';
 
 import type { Player } from 'src/dto/game.dto';
@@ -7,11 +7,13 @@ import type { Player } from 'src/dto/game.dto';
 export class InviteController {
   constructor(readonly inviteService: InviteService) {}
 
+  @Header('Accept', 'application/json')
   @Post('create')
-  create_code(@Body() host: Player): String {
-    return this.inviteService.create_code(host);
+  create_code(@Body() host: Player) {
+    this.inviteService.register_code(host);
   }
 
+  @Header('Accept', 'application/json')
   @Post('find')
   find_by_code(@Body() invite_code: { code: String }): Player {
     return this.inviteService.find_by_code(invite_code.code);
--- backend/src/invite/invite.invites-handler.ts
+++ backend/src/invite/invite.invites-handler.ts
@@ -1,16 +1,17 @@
 // Types
+import { BadRequestException } from '@nestjs/common';
 import type { Player } from 'src/dto/game.dto';
 
 export default class InvitesHandler {
-  private static invites = new Set<{ player: Player; code: String }>();
-  private static invites_collection: { player: Player; code: String }[] = [];
+  private static invites = new Map<String, Player>();
 
   public static store_invite(player: Player, code: String) {
-    this.invites_collection.push({ player: player, code: code });
+    if (!this.invites.has(code)) {
+      this.invites.set(code, player);
+    } else throw new BadRequestException();
   }
 
   public static get_player_by_code(code: String): Player {
-    return this.invites_collection.find((invite) => invite.code === code)
-      .player;
+    return this.invites.get(code);
   }
 }
