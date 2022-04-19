ad00a57b404a495c9f1bcf8c6e989702f678ab43
Maximilian Trimarchi <maxipaltri@gmail.com>
2022-04-19 15:18:38 +0200

    update cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Current cors --> localhost:8080
-  app.enableCors({ origin: 'http://localhost:8080', methods: '*' });
+  app.enableCors({ origin: '*', methods: 'GET, POST' });
   await app.listen(13124);
 }
 bootstrap();
