2810d6e3d78aeb9bdf60ed3dc5984a1069a7f99d
Maximilian Trimarchi <maxipaltri@gmail.com>
2022-04-19 15:27:56 +0200

    invites route improved

--- backend/src/invite/invite.service.ts
+++ backend/src/invite/invite.service.ts
@@ -1,20 +1,16 @@
 // Nest Injectable
 import { Injectable } from '@nestjs/common';
-// Utility
+// Random String
 import { randString } from 'src/utils/utils';
-// Types
-import type { Player } from 'src/dto/game.dto';
 // Invites Handler
 import InvitesHandler from './invite.invites-handler';
+// Transfer Object Types
+import type { Player } from 'src/dto/game.dto';
 
 @Injectable()
 export class InviteService {
-  private p_code_list: { player: Player; code: String }[] = [];
-
-  create_code(host: Player): String {
-    const new_code = randString(10);
-    InvitesHandler.store_invite(host, new_code);
-    return new_code;
+  register_code(host: Player) {
+    InvitesHandler.store_invite(host, host.token);
   }
 
   find_by_code(code: String): Player {
