2ca1a9b3b6cf0084dca69f1170431e9046550082
Maximilian Trimarchi <maxipaltri@gmail.com>
2022-04-20 16:16:27 +0200

    "ShipTyp" added

--- frontend/src/dto/game.dto.ts
+++ frontend/src/dto/game.dto.ts
@@ -1,7 +1,9 @@
+import type { Ship } from "../enumerators/game.enums";
 import type { ActionStatus } from "../enumerators/events.enums";
 
 export type Action = { raw_pow: number; destination_id: String };
 export type ActionRaw = { raw_pos: number };
-export type ActionCallBack = { action_status: ActionStatus; sink: { ship_name: String } | false };
+export type ShipType = Ship | String;
+export type ActionCallBack = { action_status: ActionStatus; sink: { ship_name: ShipType } | false; lost: Boolean };
 export type Player = { name: String; token: String };
 export type FirstRequest = { host_id: String; client_id: String };
